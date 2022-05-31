cce02af2eaf48d921fb618f2491573121558bb8e
David Lev <42866208+david-lev@users.noreply.github.com>
2022-05-31 14:05:42 +0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by David Lev on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
